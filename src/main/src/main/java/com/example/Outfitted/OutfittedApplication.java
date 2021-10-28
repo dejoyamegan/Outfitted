a53b81d9ed156209fe5675a70562915a151d5c19
WillAG <antoniogutie@wisc.edu>
2021-10-27 19:59:49 -0500

    restructure of project directory. Checking if .gitignore is correctly ignoring intended files from multiple directories

---
+++ src/main/src/main/java/com/example/Outfitted/OutfittedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Outfitted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfittedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutfittedApplication.class, args);
+	}
+
+}
---
+++ src/main/src/test/java/com/example/Outfitted/OutfittedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Outfitted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfittedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
