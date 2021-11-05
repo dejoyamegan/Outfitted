9a35abced4a94cee00e713a2e09f1d2ec55e4d84
Mira <mpsubramania@wisc.edu>
2021-11-04 21:22:30 -0500

    changed to react native image collage

--- src/client/components/outfits/outfits.js
+++ src/client/components/outfits/outfits.js
@@ -4,11 +4,12 @@ import firebase from '../../firebase';
 import NavBar from '../common/navbar';
 import { Button, Collection, SegmentedControl, RowItem, TabBar} from 'react-native-ios-kit';
 import { Card, ListItem, Container } from 'react-native-elements'
+import { DynamicCollage, StaticCollage } from "react-native-images-collage";
 import { ReactPhotoCollage } from "react-photo-collage";
 const photos = [
-    { source: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2021%2F07%2Fnike-dunk-high-womens-sneakers-aluminum-baby-blue-white-price-release-date-1.jpg?q=75&w=800&cbr=1&fit=max' },
-    { source: 'https://lsco.scene7.com/is/image/lsco/290370014-front-pdp?$qv_desktop_bottoms$' },
-    { source: 'https://di2ponv0v5otw.cloudfront.net/posts/2020/08/01/5f25afd9284e99d2de7607c3/m_5f25afec163df4604b30a4ca.jpg' },
+   'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2021%2F07%2Fnike-dunk-high-womens-sneakers-aluminum-baby-blue-white-price-release-date-1.jpg?q=75&w=800&cbr=1&fit=max' ,
+    'https://lsco.scene7.com/is/image/lsco/290370014-front-pdp?$qv_desktop_bottoms$' ,
+     'https://di2ponv0v5otw.cloudfront.net/posts/2020/08/01/5f25afd9284e99d2de7607c3/m_5f25afec163df4604b30a4ca.jpg' 
   ]
 const setting = {
     width: '300px',
@@ -47,8 +48,7 @@ export default class Outfits extends Component {
             <View style={styles.container}>
                
                 <Card>
-                 <ReactPhotoCollage {...setting}
-                 />
+                <StaticCollage width={400} height={400} images={photos} matrix={[1, 1, 1]} />
                  <View style={{ marginVertical: 10 }}>
                  <Button style={styles.button} inline rounded>
                     Delete Outfit
