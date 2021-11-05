a84d6ec529510719e6d63d166ad7feb178d2dd23
Mira <mpsubramania@wisc.edu>
2021-11-04 20:22:56 -0500

    added photo collage to card + delete button

--- src/client/components/outfits/outfits.js
+++ src/client/components/outfits/outfits.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput,  Alert, ActivityIndicator } from 'react-native';
+import React, {Component, useState} from 'react';
+import { StyleSheet, Picker, Text, View, TextInput,  Alert, ActivityIndicator } from 'react-native';
 import firebase from '../../firebase';
 import NavBar from '../common/navbar';
 import { Button, Collection, SegmentedControl, RowItem, TabBar} from 'react-native-ios-kit';
 import { Card, ListItem, Container } from 'react-native-elements'
 import { ReactPhotoCollage } from "react-photo-collage";
-
+const photos = [
+    { source: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2021%2F07%2Fnike-dunk-high-womens-sneakers-aluminum-baby-blue-white-price-release-date-1.jpg?q=75&w=800&cbr=1&fit=max' },
+    { source: 'https://lsco.scene7.com/is/image/lsco/290370014-front-pdp?$qv_desktop_bottoms$' },
+    { source: 'https://di2ponv0v5otw.cloudfront.net/posts/2020/08/01/5f25afd9284e99d2de7607c3/m_5f25afec163df4604b30a4ca.jpg' },
+  ]
 const setting = {
-    width: '600px',
-    height: ['250px', '170px'],
+    width: '300px',
+    height: ['100px', '100px'],
     layout: [1, 4],
     photos: [
       { source: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2021%2F07%2Fnike-dunk-high-womens-sneakers-aluminum-baby-blue-white-price-release-date-1.jpg?q=75&w=800&cbr=1&fit=max' },
@@ -17,6 +21,7 @@ const setting = {
     ],
     showNumOfRemainingPhotos: true
   };
+
 export default class Outfits extends Component {
 
 
@@ -40,7 +45,16 @@ export default class Outfits extends Component {
         return(
 
             <View style={styles.container}>
-                 <ReactPhotoCollage {...setting} />
+               
+                <Card>
+                 <ReactPhotoCollage {...setting}
+                 />
+                 <View style={{ marginVertical: 10 }}>
+                 <Button style={styles.button} inline rounded>
+                    Delete Outfit
+                </Button>
+                </View>
+                 </Card>
                  <View style={{ marginVertical: 10 }}>
                  <Button style={styles.button} inline rounded>
                     Add Item
