95e8b36ab849f6cf0a92c29db4027924eec41506
jaclynkeilen <jkeilen@wisc.edu>
2021-11-16 20:45:09 -0600

    Added tests/code coverage for category, closet, item, outfit, and user objects

--- src/main/src/main/java/com/example/Outfitted/Objects/Category.java
+++ src/main/src/main/java/com/example/Outfitted/Objects/Category.java
@@ -23,21 +23,26 @@ public class Category {
 	
 	// getters and setters
 	public void setName(String newName) {
+
 		this.name = newName;
 	}
 	public String getName() {
+
 		return this.name;
 	}
 	public ArrayList<Item> getItems(){
+
 		return items;
 	}
 	
 	// clothing category functions
 	public void addItem(Item newItem) {
+
 		items.add(newItem);
 	}
 	
 	public boolean removeItem(Item itemToRemove) {
+
 		return items.remove(itemToRemove);
 	}
 }
--- src/main/src/main/java/com/example/Outfitted/Users/User.java
+++ src/main/src/main/java/com/example/Outfitted/Users/User.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class User {
     private String name;
-
     private String age;
-
     private String city;
 
     public User() {
@@ -22,29 +20,33 @@ public class User {
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getAge() {
+
         return age;
     }
 
     public void setAge(String age) {
+
         this.age = age;
     }
 
     public String getCity() {
+
         return city;
     }
 
     public void setCity(String city) {
+
         this.city = city;
     }
-
-
 }
 
